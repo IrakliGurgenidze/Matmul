a6a2d0b8d7b21ceb38bc9399643346b600627f5d
griffinr <griffinravo@gmail.com>
2025-03-13 18:43:21 -0400

    Read R1 + R2, Build Ai and Ci, Group by b
    Added combine logic.

--- main.cpp
+++ main.cpp
@@ -1,48 +1,148 @@
-#include <stdlib>
+#include <vector>
+
+double EPSILON = 0.1;
+int K_VAL = 9 / EPSILON;
+int P_VAL = 1;
 
 struct R1Tuple {
   int a;
   int b;
   double h1a;
-}
+};
 
 struct R2Tuple {
   int b;
   int c;
   double h2c;
-}
+};
+
+struct ACpair {
+    int a;
+    int c;
+    double hashAC;
+};
 
 //TODO: pairwise hashing
 double hash(int x) {
   return x;
 }
 
+double hashAC(double h1a, double h2c){
+    return h1a-h2c;
+}
+
 int main() {
 
-  std::vector<vector<int>> R1 = {{1, 0, 0}, {1, 1, 0}, {0, 0, 1}};
-  std::vector<vector<int>> R2 = {{0, 1, 0}, {1, 0, 1}, {0, 0, 1}};
+    std::vector<std::vector<int> > R1 = {{1, 0, 0}, {1, 1, 0}, {0, 0, 1}};
+    std::vector<std::vector<int> > R2 = {{0, 1, 0}, {1, 0, 1}, {0, 0, 1}};
 
-  std::vector<R1Tuple> R1Tuples;
-  std::vector<R2Tuple> R2Tuples;
+    std::vector<R1Tuple> R1Tuples;
+    std::vector<R2Tuple> R2Tuples;
 
-  for (int a = 0; a < R1.size(); a++) {
-    for (int b = 0; b < R1[a].size(); b++) {
-      if(R1[a][b] == 1) {
-        double hashVal = hash(a);
-        R1Tuples.push_back({a, b, hashVal});
-      }
+    // R1 -> (a,b,h1(a))
+    for (int a = 0; a < R1.size(); a++) {
+        for (int b = 0; b < R1[a].size(); b++) {
+          if(R1[a][b] == 1) {
+            double hashVal = hash(a);
+            R1Tuples.push_back({a, b, hashVal});
+          }
+        }
     }
-  }
 
-  for (int b = 0; b < R1.size(); b++) {
-    for (int c = 0; c < R1[b].size(); c++) {
-      if(R1[b][c] == 1) {
-        double hashVal = hash(c);
-        R1Tuples.push_back({b, c, hashVal});
-      }
+    //R2 -> (b,c,h2(c))
+    for (int b = 0; b < R2.size(); b++) {
+        for (int c = 0; c < R2[b].size(); c++) {
+          if(R1[b][c] == 1) {
+            double hashVal = hash(c);
+            R2Tuples.push_back({b, c, hashVal});
+          }
+        }
     }
-  }
 
-  return 0;
 
+    // Sorting primarily by b then by hash val
+    sort(R1Tuples.begin(), R1Tuples.end(), [](const R1Tuple &lhs, const R1Tuple &rhs) {
+      if(lhs.b != rhs.b) return lhs.b < rhs.b;
+      return lhs.h1a < rhs.h1a;
+    });
+
+    sort(R2Tuples.begin(), R2Tuples.end(), [](const R2Tuple &lhs, const R2Tuple &rhs) {
+        if(lhs.b != rhs.b) return lhs.b < rhs.b;
+        return lhs.h2c < rhs.h2c;
+    });
+
+    //Grouping by b for all groups of b=i
+    std::vector<std::pair<int,std::vector<R1Tuple>>> Ai;
+    int start = 0;
+
+    while(start < R1Tuples.size()){
+        int currB = R1Tuples[start].b;
+        int end = start+1;
+
+        while(end < R1Tuples.size() && R1Tuples[end].b == currB){
+            end++;
+        }
+
+        std::vector<R1Tuple> bGroup(R1Tuples.begin() + start, R1Tuples.begin() + end);
+        Ai.push_back({currB, bGroup});
+
+        start = end;
+    }
+
+    std::vector<std::pair<int,std::vector<R2Tuple>>> Ci;
+    start = 0;
+
+    while(start < R1Tuples.size()){
+        int currB = R2Tuples[start].b;
+        int end = start+1;
+
+        while(end < R1Tuples.size() && R2Tuples[end].b == currB){
+            end++;
+        }
+
+        std::vector<R2Tuple> bGroup(R2Tuples.begin() + start, R2Tuples.begin() + end);
+        Ci.push_back({currB, bGroup});
+
+        start = end;
+    }
+
+
+    //TODO: Start psuedocode process to estimate size now that we have Ai and Ci sorted properly
+
+    return 0;
+
+}
+
+//TODO: Make combine return similar to psuedocode?
+void combine(std::vector<ACpair> &S, std::vector<ACpair> &F) {
+
+    // h(S) union h(F)
+    for(auto &x: F) S.push_back(x);
+    F.clear();
+
+    // No kth smallest val if size < k
+    if(S.size() < K_VAL) return;
+
+
+    // "RANK": split S with nth element func, lhs < k (at proper idx) < rhs
+    // psuedocode: gives us 'v'
+    std::nth_element(S.begin(), S.begin() + (K_VAL-1),S.end(),
+                     [](const ACpair &lhs, const ACpair &rhs) {
+        return lhs.hashAC < rhs.hashAC;
+    });
+
+    double thresh = (S.begin() + (K_VAL-1)) -> hashAC;
+
+    // x in S where h(x) <= v
+    auto mid = std::partition(S.begin(), S.end(),
+                         [&](auto &x){return x.hashAC <= thresh;}
+    );
+    S.erase(mid, S.end());
+
+    // if there are more than k items <= newp, keep exactly k
+    if ((int)S.size() > K_VAL) {
+        S.resize(K_VAL);
+    }
+
+    P_VAL = thresh;
 }
