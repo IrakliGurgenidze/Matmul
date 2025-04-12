2d17bf27be9da8f34509cb172122f5709e9747b8
IrakliGurgenidze <rocky.gurgenidze@gmail.com>
2025-04-12 17:35:18 -0400

    Estimator timing test written

--- tests/TestEstimator.cpp
+++ tests/TestEstimator.cpp
@@ -4,6 +4,50 @@
 #include "../include/Estimator.h"
 #include "../include/MatrixUtils.h"
 #include "../include/HashUtils.h"
+#include "../CoordListMatrix.h"
+
+#include <chrono>
+
+
+void benchmark_estimator(
+    int M, int K, int N,
+    double sparsity,
+    const std::string& label,
+    bool csv=false
+) {
+    // std::random_device rd;
+    int seedA = 1;
+    int seedB = 2;
+
+    const auto coordsA = generateSparseMatrix(sparsity, M, K, seedA);
+    const auto coordsB = generateSparseMatrix(sparsity, K, N, seedB);
+
+    CoordListMatrix A(coordsA, M, K);
+    CoordListMatrix B(coordsB, K, N);
+
+    const auto start = std::chrono::steady_clock::now();
+    double estimation = estimateProductSize(A.getHashedCoords(), B.getHashedCoords());
+    const auto end = std::chrono::steady_clock::now();
+
+    int trueNumberNonzero = groundTruthCalc(A.getCoords(), B.getCoords());
+
+    const auto elapsed = std::chrono::duration<double>(end - start).count();
+    REQUIRE(elapsed > 0);
+
+    if (!csv) {
+        std::cout << std::fixed << std::setprecision(6);
+        std::cout << std::left
+                  << std::setw(21) << label
+                  << std::right
+                  << "  Estimation=" << std::setw(15) << estimation
+                  << "  True NNZ=" << std::setw(15) << trueNumberNonzero
+                  << "  time=" << std::setw(10) << elapsed << " s"
+                  << std::endl;
+    } else {
+        std::cout << estimation << "," << trueNumberNonzero << "," << elapsed << std::endl;
+    }
+}
+
 
 TEST_CASE("Estimator returns k^2 when sketch cannot be filled", "[Estimator]") {
     // This test creates a small dataset with only 4 output (a, c) pairs:
@@ -73,5 +117,20 @@ TEST_CASE("Estimator returns accurate value when sketch fills", "[Estimator][Int
         REQUIRE(estimate <= upper);
     } else {
         REQUIRE(estimate <= static_cast<double>(k * k));
+    }
+}
+
+TEST_CASE("Estimator run time", "[Estimator][Benchmark]") {
+    double sparsity = 0.0005;
+    int start_N = 10000;
+    int end_N = 100000;
+    int step = 10000;
+
+    for (int N = start_N; N <= end_N; N += step) {
+        int M = N;
+        int K = N; // or N, if you want square multiplications
+
+        std::string label = "[estimator-sweep N=" + std::to_string(N) + "]";
+        benchmark_estimator(M, K, N, sparsity, label, true);
     }
 }
